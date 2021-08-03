ec21f3a9ceb3db7c83150cfcfbee77002e8afb04
techlancesoftware <techlancesoftware@gmail.com>
2021-08-03 13:50:43 +0530

    push2

---
+++ app/src/main/java/com/example/svasth/Prevalent/Prevalent.java
@@ -0,0 +1,8 @@
+package com.example.svasth.Prevalent;
+
+import com.example.svasth.Model.Users;
+
+public class Prevalent
+{
+    private static Users currentonlineUser;
+}
--- app/src/main/java/com/example/svasth/loginActivity.java
+++ app/src/main/java/com/example/svasth/loginActivity.java
@@ -1,14 +1,130 @@
 package com.example.svasth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.svasth.Model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class loginActivity extends AppCompatActivity {
 
+    private EditText InputPhoneNumber, InputPassword;
+    private Button LoginButton;
+    private ProgressDialog loadingBar;
+
+    private String parentDbName= "Users";
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate (savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LoginButton = (Button) findViewById(R.id.splash_button_login) ;
+        InputPassword = (EditText) findViewById(R.id.login_password_hint);
+        InputPhoneNumber = (EditText) findViewById(R.id.login_phone_number);
+        loadingBar = new ProgressDialog(this);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
     }
+
+
+
+    private void loginUser()
+    {
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(phone))
+        {
+            Toast.makeText(this,"Please Enter Your Mobile Number",Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this,"Please Enter Your Password",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Login Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+
+            AllowAccessAccount(phone, password);
+        }
+
+    }
+
+    private void AllowAccessAccount(String phone, String password)
+    {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.child(parentDbName).child(phone).exists())
+                {
+                    Users usersData = dataSnapshot.child(parentDbName) .child(phone) .getValue(Users.class);
+
+                    if (usersData.getPhone().equals(phone))
+                    {
+                        if (usersData.getPassword().equals(password))
+                        {
+                            Toast.makeText(loginActivity.this, "Logged in successfuly...", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                            Intent intent = new Intent(loginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+
+                        }
+                        else
+                        {
+                            loadingBar.dismiss();
+                            Toast.makeText(loginActivity.this, "Password is incorrect.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    Toast.makeText(loginActivity.this, "Account with this" + phone + "number do not exists", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    Toast.makeText(loginActivity.this, "You need to create a new Account.", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
