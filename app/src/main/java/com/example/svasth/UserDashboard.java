9fba7b43a89b06f18cdbdceeed7046a8e9da09ae
techlancesoftware <techlancesoftware@gmail.com>
2021-08-13 14:52:35 +0530

    .

--- app/src/main/java/com/example/svasth/UserDashboard.java
+++ app/src/main/java/com/example/svasth/UserDashboard.java
@@ -2,6 +2,8 @@ package com.example.svasth;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,8 +12,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.CollapsibleActionView;
 import android.view.MenuItem;
-import android.view.View;
+
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -21,12 +25,61 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DatabaseError;
 
 public class UserDashboard extends AppCompatActivity {
-
+    public ImageView bed;
+    public ImageView hospital;
+    public ImageView helpline;
+    public ImageView care;
+    public ImageView essentials;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+
+        bed= (ImageView) findViewById(R.id.bed);
+        bed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(UserDashboard.this,Bed.class);
+                startActivity(intent);
+            }
+        });
+
+        hospital= (ImageView) findViewById(R.id.hospital);
+        hospital.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(UserDashboard.this,hospitals.class);
+                startActivity(intent);
+            }
+        });
+
+        helpline= (ImageView) findViewById(R.id.helpline);
+        helpline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(UserDashboard.this,helpline.class);
+                startActivity(intent);
+            }
+        });
+
+        care= (ImageView) findViewById(R.id.careCenter);
+        care.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(UserDashboard.this,care.class);
+                startActivity(intent);
+            }
+        });
+
+        essentials= (ImageView) findViewById(R.id.essentials);
+        essentials.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(UserDashboard.this,essentials.class);
+                startActivity(intent);
+            }
+        });
 
 
 
